30020d4a6ef4c866e0ebb56e33c7a6512a9f9e97
tom-englert <mail@tom-englert.de>
2020-12-31 15:13:30 +0100

    Add simple IntegrationTest project

---
+++ src/IntegrationTest/Build.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild" -restore /t:Rebuild /p:platform="Any CPU" /p:configuration="Release" /p:VisualStudioVersion="16.0"
+Pause
---
+++ src/IntegrationTest/IntegrationTest/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntegrationTest
+{
+    public class Class1
+    {
+        public void M1(string value1, object value2)
+        {
+
+        }
+
+        public void M2(object? value)
+        {
+            var text = value?.ToString();
+            
+            if (text == null)
+                return;
+
+            M1(text, value);
+        }
+    }
+}
