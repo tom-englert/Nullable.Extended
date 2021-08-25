29a6b663d27f1b1ebaa3f6d58feae084c4ce0e43
tom-englert <mail@tom-englert.de>
2021-08-25 19:47:55 +0200

    #10: Prove of concept

--- src/Nullable.Extended.Extension/Nullable.Extended.Extension/Analyzer/NullForgivingAnalysisResult.cs
+++ src/Nullable.Extended.Extension/Nullable.Extended.Extension/Analyzer/NullForgivingAnalysisResult.cs
@@ -1,18 +1,43 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Nullable.Extended.Extension.AnalyzerFramework;
+using TomsToolbox.Essentials;
 
 namespace Nullable.Extended.Extension.Analyzer
 {
     public class NullForgivingAnalysisResult : AnalysisResult<PostfixUnaryExpressionSyntax>, INotifyPropertyChanged, IComparable<NullForgivingAnalysisResult>
     {
+        const string SuppressionCommentPrefix = "// ! ";
+
         public NullForgivingAnalysisResult(AnalysisContext analysisContext, PostfixUnaryExpressionSyntax token, NullForgivingContext context)
             : base(analysisContext, token, token.OperatorToken.GetLocation())
         {
             Context = context;
+
+            var declaration = token
+                .AncestorsAndSelf()
+                .OfType<CSharpSyntaxNode>()
+                .FirstOrDefault(i => i is StatementSyntax || i.GetType().Name.EndsWith("DeclarationSyntax"));
+
+            if (declaration == null || !declaration.HasLeadingTrivia)
+                return;
+
+            Justification = string.Join(Environment.NewLine, declaration.GetLeadingTrivia()
+                .Where(t => t.Kind() == SyntaxKind.SingleLineCommentTrivia)
+                .Select(t => t.ToString())
+                .Reverse()
+                .TakeWhile(s => s.StartsWith(SuppressionCommentPrefix))
+                .Select(s => s.Substring(SuppressionCommentPrefix.Length))
+                .Reverse());
         }
+
+        public string? Justification { get; }
+
+        public bool IsJustified => !Justification.IsNullOrEmpty();
 
         public NullForgivingContext Context { get; set; }
 
